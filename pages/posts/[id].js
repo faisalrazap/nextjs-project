6af4f675714592bc01a55fd9c248751d61a541e1
Faisal Raza <faisalraza.p@gmail.com>
2025-07-07 17:12:41 +0500

    Added remark and remark-html libraries

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -8,6 +8,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -15,7 +17,9 @@ export default function Post({ postData }) {
 import { getAllPostIds, getPostData } from '../../lib/posts';
  
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+ 
   return {
     props: {
       postData,
